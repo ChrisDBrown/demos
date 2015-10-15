487050b21da01c3405e149844dbae007d1e1bf73
Chris Brown <chris@greenhilldigital.com>
2015-10-15 23:41:00 +0100

    Only show errors after a first submission attempt

--- angularjs-validate-on-submit/js/index.js
+++ angularjs-validate-on-submit/js/index.js
@@ -3,7 +3,9 @@ angular
         .controller('IndexController', [function() {
             var self = this;
 
+            self.submitted = false;
             self.submit = function(form) {
+                self.submitted = true;
                 if (form.$valid) {
                     console.log(self.user);
                 }
