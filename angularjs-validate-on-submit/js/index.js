ef7e515953c81ae52e89408b6a010cb5809b0929
Chris Brown <chris@greenhilldigital.com>
2015-10-15 23:38:03 +0100

    Add form submission handling

--- angularjs-validate-on-submit/js/index.js
+++ angularjs-validate-on-submit/js/index.js
@@ -1,5 +1,11 @@
 angular
     .module('demo', [])
         .controller('IndexController', [function() {
+            var self = this;
 
+            self.submit = function(form) {
+                if (form.$valid) {
+                    console.log(self.user);
+                }
+            };
 }]);
